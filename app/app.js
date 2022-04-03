9b5c76f2c0be5e89c4fc1ce71267eb462bc9ca70
Yang Bo Gyu <didqhrb1@gamil.com>
2022-04-03 23:22:41 +0900

    파일 분리화

---
+++ app/app.js
@@ -0,0 +1,14 @@
+"use strict";
+//모듈
+const express = require("express");
+const app = express();
+//라우팅
+const home = require("./src/routes/home");
+
+// app 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use -> 미들웨어 등록 메서드
+
+module.exports = app;
